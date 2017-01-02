4cafdceb71acf5c87a6f62568e9f1f747f979e99
Candid Dauth <cdauth@cdauth.eu>
2017-01-02 02:38:18 +0100

    Start session dbus

--- start.sh
+++ start.sh
@@ -2,6 +2,9 @@
 rm -f /run/dbus/pid
 rm -rf /tmp/pulse-*
 dbus-daemon --system --fork
+eval "$(dbus-launch)"
+export DBUS_SESSION_BUS_ADDRESS
+export DBUS_SESSION_BUS_PID
 pulseaudio --log-level=1 &
 /usr/lib/bluetooth/bluetoothd --plugin=a2dp -n &
 avahi-daemon -D
